31ae3852438d48f69f7e6215ec2e29f7477fb51e
NeoJite <akpojotor.onajite@gmail.com>
2022-09-11 13:33:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a [iece of art,\n");
+		return (0);
+}
