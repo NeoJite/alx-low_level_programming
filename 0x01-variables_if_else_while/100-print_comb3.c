ccd7cc39f33b9b290a99a60bdeace1263b509bf6
NeoJite <akpojotor.onajite@gmail.com>
2022-09-12 20:40:22 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = '0';
+		while (b <= '9')
+		{
+			putchar (a);
+			putchar (b);
+			if (a == '9' && b == '9')
+			{
+				break;
+			}
+			putchar (',');
+			putchar (' ');
+			b += 1;
+		}
+		a += 1;
+	}
+	putchar('\n');
+	return (0);
+}
