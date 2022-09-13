2c1edb33acb1863c952fc10c802b8f00b228eaa5
NeoJite <akpojotor.onajite@gmail.com>
2022-09-12 19:33:24 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	az = 'a';
+	while (az <= 'z')
+	{
+		if ((az != 'q') && (az != 'e'))
+		{
+			putchar(az);
+		}
+		az = az + 1;
+	}
+	putchar('\n');
+	return (0);
+}
