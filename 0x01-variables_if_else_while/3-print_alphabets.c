4dbcc35ff8ab90a6ec4eff3705087e9ceeb0a30c
NeoJite <akpojotor.onajite@gmail.com>
2022-09-12 19:23:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+	char AZ;
+
+	az = 'a';
+	while (az <= 'z')
+	{
+		putchar(az);
+		az = az + 1;
+	}
+
+	AZ = 'A';
+	while (AZ <= 'Z')
+	{
+		putchar(AZ);
+		AZ = AZ + 1;
+	}
+	putchar('\n');
+	return (0);
+}
