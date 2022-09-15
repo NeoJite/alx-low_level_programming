cc2b60cc4b5912fe19a5668d17173012ee101ab3
NeoJite <akpojotor.onajite@gmail.com>
2022-09-14 17:11:15 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 int _islower(int c);
+void print_alphabet_x10(void);
+int _isalpha(int c);
 
 #endif
