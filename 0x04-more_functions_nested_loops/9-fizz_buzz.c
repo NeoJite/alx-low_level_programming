f04a8e0fb882e56230ffd0a18bf1db5bfcabf322
NeoJite <akpojotor.onajite@gmail.com>
2022-09-16 11:13:26 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,30 @@
+#include <stdio>
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - entry point
  *
- * Return: Always 0.
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int a;
+	int 1;
 
-	for (a = 1; a <= 100; a++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		}
-		else if (a % 3 == 0)
-		{
+		else if (i % 3 == 0)
 			printf("Fizz");
-		}
-		else if (a % 5 == 0)
-		{
+		else if (i % 5 == 0)
 			printf("Buzz");
-		}
 		else
-			printf("%d", a);
-		if (a != 100)
+			printf("%i", i);
+		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
