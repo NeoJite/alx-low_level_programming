b74799db2270f4d927388d384ae9b53fa0498774
NeoJite <akpojotor.onajite@gmail.com>
2022-09-15 18:13:13 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: any first number to multiplies
+ * @b: any second number to multiplies
+ * Return: "a" and "b"
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
