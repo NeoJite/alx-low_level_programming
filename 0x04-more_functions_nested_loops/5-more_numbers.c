77a7f4fa556e2603f81eae1cbdd09c079d4e73df
NeoJite <akpojotor.onajite@gmail.com>
2022-09-15 19:11:46 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,16 @@
  */
 void more_numbers(void)
 {
-	int a;
-	int b;
+	int a, b;
 
-	 for (a = 0; a <= 9; a++)
-	 {
-		 for (b = 0; b <= 14; b++)
-		 {
-			 if (b > 9)
-				 _putchar((b / 10) + '0');
-			 _putchar((b % 10) + '0');
-		 }
-	 _putchar('\n');
-	 }
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar(b / 10 % '0');
+		}
+		_putchar('\n');
+	}
 }
