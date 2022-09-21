6a4d7b5726faa1c94a69938c46c4496c648c988a
NeoJite <akpojotor.onajite@gmail.com>
2022-09-20 19:57:37 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void print_rev(char *s);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void rev_string(char *s);
+void puts_half(char *str);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
