a7bb57ed2b06353b19aa07e5795225bc479554cb
NeoJite <akpojotor.onajite@gmail.com>
2022-09-22 19:11:09 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a poinnter to
+ * an int and updates the value it points to
+ * @n: parameter of the function
+ * Return: void
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
