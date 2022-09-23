a20d2386192f27f4e3103b37b3a7618a7bcccced
NeoJite <akpojotor.onajite@gmail.com>
2022-09-22 20:17:43 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,41 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * reset_to_98 - function that takes a poinnter to
- * an int and updates the value it points to
- * @n: parameter of the function
- * Return: void
- *
+ * _strlen - function that return the lenght of a string
+ * @s: the string
+ * Return: length of s
  */
-void reset_to_98(int *n)
-{
-	*n = 98;
 
-	return (0);
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int ls, ld;
+
+	ld = _strlen(dest);
+	ld = _strlen(dest);
+	while (ls >= 0)
+	{
+		*(dest + ld + ls) = *(src + ls);
+		ls--;
+	}
+	*(dest + _strlen(dest)) = '\0';
+
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,16 +4,17 @@
 #include <stdio.h>
 
 int _putchar(char);
-void reset_to_98(int *n);
-void swap_int(int *, int *);
-int _strlen(char *);
-void _puts(char *);
-void print_rev(char *);
-void rev_string(char *);
-void puts2(char *);
-void puts_half(char *);
-void print_array(int *, int);
-char *_strcpy(char *, char *);
-int _atoi(char *);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int);
+char *infinite_add(char *, char *, char *, int);
+void print_buffer(char *, int);
 
 #endif
