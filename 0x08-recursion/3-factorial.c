c76080017ffb3978d15a094af3662e839dbabfac
NeoJite <akpojotor.onajite@gmail.com>
2022-09-28 21:18:52 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,13 +10,13 @@
 
 int factorial(int n)
 {
-	int r;
+	int next;
 
 	if (n == 0)
 		return (1);
 	else if (n < 0)
 		return (-1);
 
-	r = factorial(n - 1);
+	next = factorial(n - 1);
 	return (n * next);
 }
