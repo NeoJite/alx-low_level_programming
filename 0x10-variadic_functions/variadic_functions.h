54e56a29f9075fef673c722167f397ade46ee9a8
NeoJite <akpojotor.onajite@gmail.com>
2022-10-13 21:43:06 -0700

    3-print_all.c
    git commit -m 3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,9 +9,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
+
+/**
+ * struct print - multiple choice print
+ * @x: char Type of print
+ * @T_func: funct
+ */
+
+typedef struct print
+{
+	char *x;
+	void (*T_func)(va_list);
+} t_print;
 
 #endif
