3de2a46c0b0bb0f0755e16f28dba2dc22b592654
NeoJite <akpojotor.onajite@gmail.com>
2022-10-28 14:37:28 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,41 +10,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (!check_valid_string(b))
-		return (0);
-
-	while (b[str_len] != '\0')
-		str_len++;
-
-	while (str_len)
-	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
-	}
-		return (decimal);
-}
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-
-int check_valid_string(const char *b)
-{
 	if (b == NULL)
 		return (0);
 
-	while (*b)
+	while (b[i + 1])
 	{
-		if (*b != '1' && *b != '0')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		b++;
+		i++;
 	}
-	return (1);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+	return (res);
 }
