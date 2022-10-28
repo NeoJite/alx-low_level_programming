593813f449e3a7af900ba3059f20fe7a9f8f591b
NeoJite <akpojotor.onajite@gmail.com>
2022-10-28 15:32:01 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness.
+ * Returns: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	char *endian = (char *)&test;
+
+	if (*endian)
+		return (1);
+
+	return (0);
+}
